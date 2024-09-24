def565427ddaa4ba39dab0bde1f738f5a9f53c87
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-24 21:40:43 +0530

    some header ui update with scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-[200vh] flex items-center justify-center">
       <div>
       <h1 className="text-3xl font-bold">Hash Pet</h1>
       <p className="text-pink-400 font-bold text-center text-3xl">soon</p>
