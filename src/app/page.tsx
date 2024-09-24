d818ac6569d85e3b6f6e0f2d9904410ee52222a8
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-24 23:35:51 +0530

    few ui and few images are fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="max-w-7xl mx-auto">
       <HeroSection/>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="sm:hidden block text-center text-lg font-semibold bg-orange-300 p-2 rounded-lg">Hero Section For Mobile Under Development</h1>
+        <h1 className="hidden sm:block text-center text-lg font-semibold bg-orange-300 p-2 rounded-lg">This Section is Under Development</h1>
+      </div>
     </div>
   );
 }
