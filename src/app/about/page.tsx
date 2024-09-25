51e0aacadc1eb359c247b028621cc45572b1a542
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-25 23:13:57 +0530

    under construction page not added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      this is the about page 
+    <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="block text-center text-lg font-semibold bg-orange-200 p-2 rounded-lg"><span className='text-green-500'>About</span> Page is Under Development</h1>
     </div>
   )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-        this is the contact page
+    <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="block text-center text-lg font-semibold bg-orange-200 p-2 rounded-lg"><span className='text-green-500'>Contact</span> Page is Under Development</h1>
     </div>
   )
 }
