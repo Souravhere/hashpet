6c06b4f6fe7ed057eb69da4e217525f90bcfe3dd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-25 23:34:47 +0530

    card component linked to the main page and beta test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HashpetComponent from "@/components/HashpetComponent";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <HeroSection/>
+      <HashpetComponent/>
       <div className="w-full h-screen flex items-center justify-center">
         <h1 className="sm:hidden block text-center text-lg font-semibold bg-orange-300 p-2 rounded-lg">Hero Section For Mobile Under Development</h1>
         <h1 className="hidden sm:block text-center text-lg font-semibold bg-orange-300 p-2 rounded-lg">This Section is Under Development</h1>
