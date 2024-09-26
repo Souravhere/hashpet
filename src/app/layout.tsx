f02c0504738582ded9bae7337397613e63cca5b0
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-26 18:40:51 +0530

    footer component created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+        <Footer/>                                               
       </body>
     </html>
   );
