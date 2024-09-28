3276affdec0f9b5eca80ec67ccb71709ac62f53c
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-09-28 14:35:33 +0530

    cta button ui color fixed

--- src/components/Ctasection.tsx
+++ src/components/Ctasection.tsx
@@ -72,7 +72,7 @@ const Ctasection = () => {
 
           <Link href="/contact">
             <motion.div
-              className="mt-4 inline-block px-6 py-2 bg-gradient-to-r from-pink-500 to-purple-500 text-white rounded-full hover:scale-105 transform transition"
+              className="mt-4 inline-block px-6 py-2 text-white bg-gradient-to-r from-[#A45489] to-[#7A69A2] rounded-xl hover:scale-105 transform transition"
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
